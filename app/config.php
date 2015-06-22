64e158b64c778e6b9eb3a9bfc3f7dc6ec71f0f48
vladimirzhukov <vladimirzh@yandex.ru>
2015-06-23 00:33:50 +0300

    Создали config.php

---
+++ app/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'domains'=>[
+        'vzcms.local'
+    ],
+    'db'=>[
+        'host'=>'localhost',
+        'user'=>'root',
+        'password'=>'',
+        'dbname'=>'vzcms'
+    ]
+]
+?>
